02180823ca471f7eb6735fd69457dab3eaa92d04
Muhan Li <hanhanmumuqq@163.com>
2019-06-04 22:07:08 +0800

    Update modules

---
+++ include/cuda_union_find/cuda_union_find.h
@@ -0,0 +1,19 @@
+#include <cuda_runtime.h>
+#include <cuda_device_runtime_api.h>
+#ifdef __JETBRAINS_IDE__
+#include <cuda_fake_headers.h>
+#endif
+
+namespace CuMeanShift {
+    template <int blk_width=32, int blk_height=32, int channels=1>
+    class CudaUnionFind {
+    public:
+        void union_find(int *labels,
+                        float *input,
+                        int *new_labels,
+                        int *label_counter,
+                        int pitch,
+                        int width,
+                        int height);
+    };
+}
