9070c676c42c15ae2530613ae3dc2a45e2ddd791
weiby3 <393245304@qq.com>
2019-06-08 18:37:39 +0800

    Refactored cuda_flooding implement with ERROR.

--- source/cuda_flooding/cuda_flooding.cu
+++ source/cuda_flooding/cuda_flooding.cu
@@ -1,4 +1,5 @@
 #include <cuda_flooding/cuda_flooding.h>
+#include <utils.h>
 
 #include <cuda_device_runtime_api.h>
 #include <cuda_runtime.h>
@@ -6,13 +7,13 @@
 #include <cuda_fake_headers.h>
 #endif
 
+// texture<float, 2, cudaReadModeElementType> in_tex; //存储图像输入
 //#define IDX(x, y, s) ((y) * (s) + (x))
-__shared__ float *neighbor_pixels; //存储邻域像素值
 
-template <int radius>
-__global__ void flooding(int *output, int channels, int pitch, int width,
-                         int height, int row_offset, int col_offset,
-                         float color_range) {
+template <int channels = 1, int radius = 4>
+__global__ void flooding(cudaTextureObject_t in_tex, int *output, int width,
+                         int height, float color_range) {
+  __shared__ float *neighbor_pixels; //存储邻域像素值
   int x = threadIdx.x + blockDim.x * blockIdx.x;
   int y = threadIdx.y + blockDim.y * blockIdx.y;
   output[x * width + y] = x * width + y;
@@ -29,7 +30,7 @@ __global__ void flooding(int *output, int channels, int pitch, int width,
           for (int k = 0; k < channels; k++) {
             float delta = neighbor_pixels[x * width + y + k] -
                           neighbor_pixels[xx * width + yy + k];
-            delta_luv += delta;
+            delta_luv += delta * delta;
           }
           if (delta_luv < color_range) {
             output[xx * width + yy] = x * width + y;
@@ -38,4 +39,18 @@ __global__ void flooding(int *output, int channels, int pitch, int width,
       }
     }
   }
+}
+
+namespace CuMeanShift {
+template <int blk_width, int blk_height, int channels, int radius>
+void CudaFlooding<blk_width, blk_height, channels, radius>::flooding(
+    cudaTextureObject_t in_tex, int *output, int width, int height,
+    float color_range) {
+  dim3 block_1(blk_width, blk_height);
+  dim3 grid_1(CEIL(width, blk_width), CEIL(height, blk_height));
+  flooding<channels, radius>
+      <<<grid_1, block_1, sizeof(float) * width * height * channels>>>(
+          in_tex, output, width, height);
+  cudaDeviceSynchronize();
+}
 }
