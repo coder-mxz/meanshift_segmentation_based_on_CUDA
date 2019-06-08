44b16553ea7fe476626c8ba485e8b3353d238a80
weiby3 <393245304@qq.com>
2019-06-08 19:10:37 +0800

    Refactored cuda_flooding implement successfully.

--- source/cuda_flooding/cuda_flooding.cu
+++ source/cuda_flooding/cuda_flooding.cu
@@ -11,14 +11,14 @@
 //#define IDX(x, y, s) ((y) * (s) + (x))
 
 template <int channels = 1, int radius = 4>
-__global__ void flooding(cudaTextureObject_t in_tex, int *output, int width,
-                         int height, float color_range) {
-  __shared__ float *neighbor_pixels; //存储邻域像素值
+__global__ void _flooding(cudaTextureObject_t in_tex, int *output, int width,
+                          int height, float color_range) {
+  __shared__ extern float neighbor_pixels[]; //存储邻域像素值
   int x = threadIdx.x + blockDim.x * blockIdx.x;
   int y = threadIdx.y + blockDim.y * blockIdx.y;
   output[x * width + y] = x * width + y;
   for (int i = 0; i < channels; i++) {
-    neighbor_pixels[x * width + y + i] = tex2D(in_tex, x, y + i);
+    neighbor_pixels[x * width + y + i] = tex2D<float>(in_tex, x, y + i);
   }
   __syncthreads();
   for (int _count = 0; _count < 20; _count++) {
@@ -48,9 +48,9 @@ void CudaFlooding<blk_width, blk_height, channels, radius>::flooding(
     float color_range) {
   dim3 block_1(blk_width, blk_height);
   dim3 grid_1(CEIL(width, blk_width), CEIL(height, blk_height));
-  flooding<channels, radius>
+  _flooding<channels, radius>
       <<<grid_1, block_1, sizeof(float) * width * height * channels>>>(
-          in_tex, output, width, height);
+          in_tex, output, width, height, color_range);
   cudaDeviceSynchronize();
 }
 }
