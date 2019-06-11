7ba1af0584e9c4e93d9a22e5126205fb96ab624e
谢江钊 <xiejzh3@mail2.sysu.edu.cn>
2019-06-11 22:06:41 +0800

    fix bug

--- include/cuda_ms_filter/cuda_ms_filter.h
+++ include/cuda_ms_filter/cuda_ms_filter.h
@@ -6,16 +6,16 @@
 
 namespace CuMeanShift {
     template <int blk_width=32, int blk_height=32, int channels=1, bool ignore_labels=false>
-    class CudaUnionFind {
+    class CudaMsFilter {
     public:
-        void union_find(float3 *img,
-                        float3 *dst,
+        void ms_filter_luv(float *input,
+                        float *output,
                         int width,
                         int height,
+                        int pitch,
                         int dis_range,
                         float color_range,
                         float min_shift,
-                        int max_iter,
-                        float3* host_res);
+                        int max_iter);
     };
 }
--- source/cuda_ms_filter/main.cpp
+++ source/cuda_ms_filter/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cuda_ms_filter.h>
 
+typedef cudaTextureObject_t i;
 using namespace std;
 using namespace CuMeanShift;
 using namespace cimg_library;
@@ -11,7 +12,7 @@ bool outputBin(const char *path, size_t count, int *data) {
     ofstream file(path, ios_base::out | ios_base::trunc | ios_base::binary);
     if (!file.is_open())
         return false;
-    file.write((const char*)&count, sizeof(size_t));
+    file.write((const char *) &count, sizeof(size_t));
     file.write((const char *) data, count * sizeof(int));
     file.close();
     return true;
@@ -26,29 +27,67 @@ bool outputBin(const char *path, size_t count, float *data) {
     ofstream file(path, ios_base::out | ios_base::trunc | ios_base::binary);
     if (!file.is_open())
         return false;
-    file.write((const char*)&count, sizeof(size_t));
-    file.write((const char*)data, count * sizeof(float));
+    file.write((const char *) &count, sizeof(size_t));
+    file.write((const char *) data, count * sizeof(float));
     file.close();
     return true;
 }
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]) {
     if (argc != 5) {
         cout << "Invalid argument number: " << argc - 1 << ", required is 4" << endl;
         return 1;
     }
-    CImg<float> img(argv[1]), org_img(img);
+    CImg<float> img(argv[1]);
     if (img.is_empty()) {
         cout << "Failed to read image" << endl;
         return 1;
-    }
-    else if (img.spectrum() != 3) {
+    } else if (img.spectrum() != 3) {
         cout << "Image should be 3-channels, get " << img.spectrum() << " channels" << endl;
         return 1;
     }
     long spatial_radius = strtol(argv[3], nullptr, 10);
     float color_radius = strtof(argv[4], nullptr);
-    int img_size=img.height()*img.width()*sizeof(float3);
+    int img_size = img.height() * img.width() * sizeof(float3);
+
+    float *cuda_input, *cuda_output, *output;
+    size_t pitch;
+    cudaMalloc((void **) &cuda_output, img.width() * img.height() * img.spectrum() * sizeof(float));
+    output = (float *) malloc(img.width() * img.height() * img.spectrum() * sizeof(float));
+    cudaMallocPitch(
+            &cuda_input,
+            &pitch,
+            img.width() * sizeof(float),
+            img.height() * img.spectrum());
+    cudaMemcpy2D(
+            cuda_input,
+            pitch,
+            img.data(),
+            img.width() * sizeof(float),
+            img.width() * sizeof(float),
+            img.height() * img.spectrum(),
+            cudaMemcpyHostToDevice);
+    CudaMsFilter<16, 16, 3, true> uf;
+    uf.ms_filter_luv(cuda_input, cuda_output, img.width(), img.height(), pitch, spatial_radius, color_radius, 5, 5);
+    cudaMemcpy(output, cuda_output, img.width() * img.height() * img.spectrum() * sizeof(float),
+               cudaMemcpyDeviceToHost);
+    CImg<float> labels_img(output, img.width(), img.height(), 1, 3);
+    CImgDisplay disp(labels_img, "labels", 2);
+    disp.show();
+    while (!disp.is_closed()) {
+        disp.wait();
+    }
+//    for (int i = 0; i < min(img.height(), 10); ++i) {
+//        for (int j = 0; j < min(img.with(), 10); ++j) {
+//            printf("%f %f %f\n", img(j, i, 0), img(j, i, 1), img(j, i, 2));
+//        }
+//    }
+    if (!outputBin(argv[2], img.size(), img.data())) {
+        cout << "Failed to output bin file" << endl;
+        return 1;
+    }
+    //cudaMemcpy(labels_h, labels, img.width() * img.height() * sizeof(int), cudaMemcpyDeviceToHost);
+    /*
     float3* host_img;
     host_img=(float3*)malloc(img_size);
     for (int i = 0; i < img.height(); ++i) {
@@ -82,5 +121,7 @@ int main(int argc, char *argv[])
         cout << "Failed to output bin file" << endl;
         return 1;
     }
+    cudaFree(cuda_img);
+    cudaFree(cuda_res);*/
     return 0;
 }
