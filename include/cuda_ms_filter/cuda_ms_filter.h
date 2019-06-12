24c18825a022d8994e1054ebee45b174cc5abed9
谢江钊 <xiejzh3@mail2.sysu.edu.cn>
2019-06-13 00:17:10 +0800

    update 3

--- include/cuda_ms_filter/cuda_ms_filter.h
+++ include/cuda_ms_filter/cuda_ms_filter.h
@@ -1,21 +1,23 @@
 #include <cuda_runtime.h>
 #include <cuda_device_runtime_api.h>
+
 #ifdef __JETBRAINS_IDE__
+
 #include <cuda_fake_headers.h>
+
 #endif
 
 namespace CuMeanShift {
-    template <int blk_width=32, int blk_height=32, int channels=1, bool ignore_labels=false>
-    class CudaUnionFind {
+    template<int blk_width = 32, int blk_height = 32, int channels = 1>
+    class CudaMsFilter {
     public:
-        void union_find(float3 *img,
-                        float3 *dst,
-                        int width,
-                        int height,
-                        int dis_range,
-                        float color_range,
-                        float min_shift,
-                        int max_iter,
-                        float3* host_res);
+        void ms_filter_luv(float *input,
+                           float *output,
+                           int width,
+                           int height,
+                           int pitch,
+                           int dis_range,
+                           float color_range,
+                           int max_iter);
     };
 }
--- source/cuda_ms_filter/cuda_ms_filter_impl.cu
+++ source/cuda_ms_filter/cuda_ms_filter_impl.cu
@@ -3,11 +3,16 @@
 //
 
 #include "cuda_ms_filter.cu"
-template class CuMeanShift::CudaUnionFind<16, 16, 3, true>;
-template class CuMeanShift::CudaUnionFind<16, 16, 3, false>;
-template class CuMeanShift::CudaUnionFind<16, 16, 1, true>;
-template class CuMeanShift::CudaUnionFind<16, 16, 1, false>;
-template class CuMeanShift::CudaUnionFind<32, 32, 3, true>;
-template class CuMeanShift::CudaUnionFind<32, 32, 3, false>;
-template class CuMeanShift::CudaUnionFind<32, 32, 1, true>;
-template class CuMeanShift::CudaUnionFind<32, 32, 1, false>;
+
+template
+class CuMeanShift::CudaMsFilter<16, 16, 3>;
+
+template
+class CuMeanShift::CudaMsFilter<16, 16, 1>;
+
+template
+class CuMeanShift::CudaMsFilter<32, 32, 3>;
+
+
+template
+class CuMeanShift::CudaMsFilter<32, 32, 1>;
